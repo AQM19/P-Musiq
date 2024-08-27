48f6bf3e971a60b5b33855ce39516db6986e91df
AQM19 <quintanalaaron@gmail.com>
2024-08-27 22:48:54 +0200

    Refactor: Refactorización del main

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request, send_file
-import re
+from pathlib import Path
 from pytube import YouTube
 import os
-from pathlib import Path
+import re
 
 app = Flask(__name__)
 
-# Set the download directory generically to the user's "Downloads" folder
-home_directory = Path.home()
-downloads_directory = home_directory / "Downloads"
+HOME_DIRECTORY = Path.home()
+DOWNLOAD_DIRECTORY = HOME_DIRECTORY / "Downloads"
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,10 +18,9 @@ def download():
     youtube_link = request.form.get('youtube_link')
     if youtube_link:
         try:
-            # Download the MP3 file
             filename = download_audio_mp3_from_youtube(youtube_link)
-            # Send the file back to the user
-            return send_file(os.path.join(downloads_directory, filename), as_attachment=True)
+            download_path = os.path.join(DOWNLOAD_DIRECTORY, filename)
+            return send_file(download_path, as_attachment=True)
         except Exception as e:
             return render_template('index.html', message=f"Error downloading audio: {e}")
     else:
@@ -33,8 +31,8 @@ def download_audio_mp3_from_youtube(url):
     claned_name = clean_file_name(yt.title)
     audio_stream = yt.streams.filter(only_audio=True).first()
     filename = f"{claned_name}.mp3"
-    downloads_directory.mkdir(parents=True, exist_ok=True)
-    audio_stream.download(output_path=downloads_directory, filename=filename)
+    DOWNLOAD_DIRECTORY.mkdir(parents=True, exist_ok=True)
+    audio_stream.download(output_path=DOWNLOAD_DIRECTORY, filename=filename)
     return filename
 
 def clean_file_name(name):
@@ -43,4 +41,8 @@ def clean_file_name(name):
     return cleaned_name 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    host = os.getenv('HOST')
+    port = os.getenv('PORT')
+    debug = os.getenv('DEBUG_MODE')
+    
+    app.run(host, port, debug)
