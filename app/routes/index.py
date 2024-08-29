25fab544b5492807156d49c4c2043b6665fa5b64
AQM19 <quintanalaaron@gmail.com>
2024-08-29 23:12:29 +0200

    Refactor: Cambio de strings literales por enumerados

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,15 +1,16 @@
 from app import Config
+from app.enums import Paths, Endpoints, TemplateNames, Messages
 from app.services.youtube_downloader import download_audio_mp3_from_youtube
 from flask import Blueprint, render_template, request, send_file
 import os
 
-index_bp = Blueprint('index', __name__)
+index_bp = Blueprint(Paths.INDEX.value, __name__)
 
-@index_bp.route('/', methods=['GET'])
+@index_bp.route(Endpoints.BASE.value, methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template(TemplateNames.INDEX.value)
 
-@index_bp.route('/download', methods=['POST'])
+@index_bp.route(Endpoints.DOWNLOAD.value, methods=['POST'])
 def download():
     youtube_link = request.form.get('youtube_link')
     if youtube_link:
@@ -17,7 +18,7 @@ def download():
             filename = download_audio_mp3_from_youtube(youtube_link)
             download_path = os.path.join(Config.DOWNLOAD_DIRECTORY, filename)
             return send_file(download_path, as_attachment=True)
-        except Exception as e:
-            return render_template('index.html', message=f"Error al descargar el audio")
+        except Exception:
+            return render_template(TemplateNames.INDEX.value, message=Messages.DOWNLOAD_ERROR.value)
 
-    return render_template('index.html', message="Por favor, inserta un link a YouTube")
+    return render_template(TemplateNames.INDEX.value, message=Messages.YOUTUBE_LINK_ERROR.value)
--- app/routes/list_bp.py
+++ app/routes/list_bp.py
@@ -1,23 +1,24 @@
 from flask import Blueprint, render_template, request, jsonify
 from app.services.youtube_downloader import map_link_list
+from app.enums import Paths, Endpoints, TemplateNames, Messages
 
-list_bp = Blueprint('list', __name__)
+list_bp = Blueprint(Paths.LIST.value, __name__)
 
-@list_bp.route('/list', methods=['GET'])
+@list_bp.route(Endpoints.LIST.value, methods=['GET'])
 def list():
-    return render_template('list.html')
+    return render_template(TemplateNames.LIST.value)
 
-@list_bp.route('/download-list', methods=['POST'])
+@list_bp.route(Endpoints.DOWNLOAD_LIST.value, methods=['POST'])
 def download_list():
     items = request.json.get('items', [])
     
     if not items:
-        return jsonify({"message": "List provided is empty"}), 400
+        return jsonify({"message": Messages.EMPTY_LIST_ERROR.value}), 400
     
     try:
         downloaded_files = map_link_list(items)
-        return jsonify({"downloaded_files": downloaded_files, "message": "Download completed successfully"}), 200
+        return jsonify({"downloaded_files": downloaded_files, "message": Messages.DOWNLOAD_SUCCESSFULL.value}), 200
     except Exception as e:
-        return jsonify({"message": f"An error occurred: {str(e)}"}), 500
+        return jsonify({"message": f"{Messages.UNEXPECTED_ERROR.value} {str(e)}"}), 500
 
 
