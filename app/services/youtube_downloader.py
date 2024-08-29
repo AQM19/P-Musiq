c4465cb8402d7db9e47cb78d3b852438055a8f9a
AQM19 <quintanalaaron@gmail.com>
2024-08-29 23:12:52 +0200

    Refactor: Cambio de strings literales por enumerados

--- app/services/youtube_downloader.py
+++ app/services/youtube_downloader.py
@@ -2,22 +2,23 @@ from pytube import YouTube
 from app.utils.utils import clean_file_name
 from flask import current_app
 import logging
+from app.enums import FileExtension, Messages
 
 def download_audio_mp3_from_youtube(url):
     try:
         yt = YouTube(url)
         cleaned_name = clean_file_name(yt.title)
         audio_stream = yt.streams.filter(only_audio=True).first()
-        filename = f"{cleaned_name}.mp3"
-        
+        filename = f"{cleaned_name}{FileExtension.MP3.value}"
+
         download_dir = current_app.config['DOWNLOAD_DIRECTORY']
         download_dir.mkdir(parents=True, exist_ok=True)
         
         audio_stream.download(output_path=str(download_dir), filename=filename)
-        logging.info(f"Successfully downloaded: {filename}")
+        logging.info(f"{Messages.DOWNLOAD_SUCCESSFULL.value} {filename}")
         return filename
     except Exception as e:
-        logging.error(f"Error downloading {url}: {str(e)}")
+        logging.error(f"{Messages.DOWNLOAD_ERROR.value} {url}: {str(e)}")
         raise
 
 def map_link_list(links):
@@ -27,6 +28,6 @@ def map_link_list(links):
             filename = download_audio_mp3_from_youtube(link)
             downloaded_files.append(filename)
         except Exception as e:
-            logging.error(f"Failed to download {link}: {str(e)}")
+            logging.error(f"{Messages.DOWNLOAD_ERROR.value} {link}: {str(e)}")
     return downloaded_files
 
