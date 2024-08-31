ad83cc9aa21056b8efefc87d98d83771f16e8b8e
AQM19 <quintanalaaron@gmail.com>
2024-08-31 12:08:46 +0200

    Refactor: Mejora en la lógica del método. Aplicación de buenas prácticas

--- app/services/youtube_downloader.py
+++ app/services/youtube_downloader.py
@@ -1,33 +1,55 @@
-from pytube import YouTube
-from app.utils.utils import clean_file_name
-from flask import current_app
-import logging
 from app.enums import FileExtension, Messages
+from app.models import Config
+from app.utils.utils import clean_file_name
+from flask import current_app, send_file, jsonify
+from pytube import YouTube
+import logging
+import os
+
+def _get_youtube_video(url):
+    """Obtiene el objeto YouTube para un URL dado."""
+    return YouTube(url)
+
+def _create_filename(yt):
+    """Crea un nombre de archivo basado en el título del video."""
+    cleaned_name = clean_file_name(yt.title)
+    return f"{cleaned_name}{FileExtension.MP3.value}"
+
+def _download_audio_stream(yt, filename, temp_dir):
+    """Descarga el flujo de audio del video de YouTube en el directorio temporal."""
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    temp_file_path = os.path.join(temp_dir, filename)
+    audio_stream.download(output_path=temp_dir, filename=filename)
+    return temp_file_path
 
 def download_audio_mp3_from_youtube(url):
+    """Descarga un archivo de audio MP3 de YouTube y lo envía como respuesta."""
     try:
-        yt = YouTube(url)
-        cleaned_name = clean_file_name(yt.title)
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        filename = f"{cleaned_name}{FileExtension.MP3.value}"
-
-        download_dir = current_app.config['DOWNLOAD_DIRECTORY']
-        download_dir.mkdir(parents=True, exist_ok=True)
+        yt = _get_youtube_video(url)
+        filename = _create_filename(yt)
+        temp_dir = Config.TEMPORARY_DIRECTORY
         
-        audio_stream.download(output_path=str(download_dir), filename=filename)
+        temp_file_path = _download_audio_stream(yt, filename, temp_dir)
         logging.info(f"{Messages.DOWNLOAD_SUCCESSFULL.value} {filename}")
-        return filename
+        
+        response = send_file(temp_file_path, as_attachment=True, download_name=filename)
+
+        os.remove(temp_file_path)
+
+        return response
+    
     except Exception as e:
         logging.error(f"{Messages.DOWNLOAD_ERROR.value} {url}: {str(e)}")
-        raise
+        return jsonify({'error': 'Error al procesar la solicitud'}), 500
 
 def map_link_list(links):
+    """Descarga archivos MP3 para una lista de enlaces de YouTube y maneja errores."""
     downloaded_files = []
     for link in links:
         try:
-            filename = download_audio_mp3_from_youtube(link)
-            downloaded_files.append(filename)
+            response = download_audio_mp3_from_youtube(link)
+            if hasattr(response, 'filename'):
+                downloaded_files.append(response.filename)
         except Exception as e:
             logging.error(f"{Messages.DOWNLOAD_ERROR.value} {link}: {str(e)}")
     return downloaded_files
-
