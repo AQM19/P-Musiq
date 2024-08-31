c238feecae21183d266898a242809627e199982e
AQM19 <quintanalaaron@gmail.com>
2024-08-31 13:44:36 +0200

    Refactor: Cambio en la lógica del endpoint para mandar de manera secuencial los archivos

--- app/routes/list_bp.py
+++ app/routes/list_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify
-from app.services.youtube_downloader import map_link_list
+from app.services.youtube_downloader import download_audio_mp3_from_youtube
 from app.enums import Paths, Endpoints, TemplateNames, Messages
 
 list_bp = Blueprint(Paths.LIST.value, __name__)
@@ -10,15 +10,14 @@ def list():
 
 @list_bp.route(Endpoints.DOWNLOAD_LIST.value, methods=['POST'])
 def download_list():
-    items = request.json.get('items', [])
-    
-    if not items:
-        return jsonify({"message": Messages.EMPTY_LIST_ERROR.value}), 400
-    
-    try:
-        downloaded_files = map_link_list(items)
-        return jsonify({"downloaded_files": downloaded_files, "message": Messages.DOWNLOAD_SUCCESSFULL.value}), 200
-    except Exception as e:
-        return jsonify({"message": f"{Messages.UNEXPECTED_ERROR.value} {str(e)}"}), 500
+    youtube_link = request.json.get('item')
+
+    if youtube_link:
+        try:
+            return download_audio_mp3_from_youtube(youtube_link)
+        except Exception as e:
+            return jsonify({"message": f"{Messages.DOWNLOAD_ERROR.value} {str(e)}"}), 500
+        
+    return render_template(TemplateNames.INDEX.value, message=Messages.YOUTUBE_LINK_ERROR.value)
 
 
