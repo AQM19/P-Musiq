0d32de37204f7dee35ec8ceb7ce6bbb5e34cfc1a
AQM19 <quintanalaaron@gmail.com>
2024-08-31 12:07:52 +0200

    Feat: Eliminación de constante inservible. Asignación de la ruta de descarga como la ruta temporal

--- app/models/config.py
+++ app/models/config.py
@@ -1,11 +1,8 @@
 import os
-from pathlib import Path
-
-HOME_DIRECTORY = Path.home()
-DOWNLOAD_DIRECTORY = HOME_DIRECTORY / "Downloads"
+import tempfile
 
 class Config:
     HOST = os.getenv('HOST', '0.0.0.0')
     PORT = int(os.getenv('PORT', 5000))
     DEBUG = os.getenv('DEBUG_MODE', 'False').lower() in ('true', '1', 't')
-    DOWNLOAD_DIRECTORY = DOWNLOAD_DIRECTORY
+    TEMPORARY_DIRECTORY = tempfile.gettempdir()
