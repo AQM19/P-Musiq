d0652c4849b63f60efd15536fe88ecc7876638e1
AQM19 <quintanalaaron@gmail.com>
2024-08-31 21:29:18 +0200

    Feat: Añadido nuevo elemento de descarga de vídeos en máxima resolución

--- app/services/youtube_downloader.py
+++ app/services/youtube_downloader.py
@@ -10,10 +10,17 @@ def _get_youtube_video(url):
     """Obtiene el objeto YouTube para un URL dado."""
     return YouTube(url)
 
-def _create_filename(yt):
+def _create_filename(yt, extension = FileExtension.MP3.value):
     """Crea un nombre de archivo basado en el título del video."""
     cleaned_name = clean_file_name(yt.title)
-    return f"{cleaned_name}{FileExtension.MP3.value}"
+    return f"{cleaned_name}.{extension}"
+
+def _download_video_stream(yt, filename, temp_dir):
+    """Descarga el flujo de video de YouTube en el directorio temporal"""
+    video_stream = yt.streams.filter(file_extension=FileExtension.MP4.value, progressive=True).get_highest_resolution()
+    temp_file_path = os.path.join(temp_dir, filename)
+    video_stream.download(output_path=temp_dir, filename=filename)
+    return temp_file_path
 
 def _download_audio_stream(yt, filename, temp_dir):
     """Descarga el flujo de audio del video de YouTube en el directorio temporal."""
@@ -41,6 +48,26 @@ def download_audio_mp3_from_youtube(url):
     except Exception as e:
         logging.error(f"{Messages.DOWNLOAD_ERROR.value} {url}: {str(e)}")
         return jsonify({'error': 'Error al procesar la solicitud'}), 500
+
+def download_video_mp4_from_youtube(url):
+    """Deacarga un archivo de video MP4 de YouTube y lo envía como respuesta"""
+    try:
+        yt = _get_youtube_video(url)
+        filename = _create_filename(yt, extension=FileExtension.MP4.value)
+        temp_dir = Config.TEMPORARY_DIRECTORY
+
+        temp_file_path = _download_video_stream(yt, filename, temp_dir)
+        logging.info(f"{Messages.DOWNLOAD_SUCCESSFULL.value} {filename}")
+
+        response = send_file(temp_file_path, as_attachment=True, download_name=filename)
+
+        os.remove(temp_file_path)
+
+        return response
+    except Exception as e:
+        logging.error(f"{Messages.DOWNLOAD_ERROR.value} {url}: {str(e)}")
+        return jsonify({'error': 'Error al procesar la solicitud'}), 500
+
 
 def map_link_list(links):
     """Descarga archivos MP3 para una lista de enlaces de YouTube y maneja errores."""
